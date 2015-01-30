689b862e6ad71073aa7750af063262bb0a5981f7
Kareem Fikry <kareem.fikry91@gmail.com>
2015-01-30 20:57:48 +0200

    fixed spacing of code files

--- 121-BestTimeToBuyAndSellStock/Solution.java
+++ 121-BestTimeToBuyAndSellStock/Solution.java
@@ -1,41 +1,22 @@
 public class Solution {
-
     public int maxProfit(int[] prices) {
-
         int min = 0;
-
         int maxDifference = 0;
-
         int buy = 0, sell = 0;
 
-        
-
         for (int i = 0; i < prices.length; i++) {
-
             if(prices[i] < prices[min]) {
-
                 min = i;
-
             }
 
             int diff = prices[i] - prices[min];
-
             if(diff > maxDifference) {
-
                 buy = min;
-
                 sell = i;
-
                 maxDifference = diff;
-
             }
-
         }
 
-        
-
         return maxDifference;
-
     }
-
 }
--- 88-MergeSortedArray/Solution.java
+++ 88-MergeSortedArray/Solution.java
@@ -1,29 +1,15 @@
 public class Solution {
-
     public void merge(int A[], int m, int B[], int n) {
-
         int pa = m - 1;
-
         int pb = n - 1;
-
         int p = m + n - 1;
 
-        
-
         while(p >= 0) {
-
             if(pb < 0 || (pa >= 0 && A[pa] > B[pb])) {
-
                 A[p--] = A[pa--];
-
             } else {
-
                 A[p--] = B[pb--];
-
             }
-
         }
-
     }
-
 }
